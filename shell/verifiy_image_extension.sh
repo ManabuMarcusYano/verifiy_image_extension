820b5ed48988c891ffed55f5d6ba4694eb0e5561
Manabu_YANO <yano_manabu@applibot.co.jp>
2016-03-31 13:38:52 +0900

    修正

--- shell/verifiy_image_extension.sh
+++ shell/verifiy_image_extension.sh
@@ -2,17 +2,17 @@
 
 filepath=$1
 ext=$(echo ${filepath##*.})
-type=$(file $filepath  | cut -d ' ' -f2)
+type=$(file "${filepath}"  | cut -d ' ' -f2)
 
 #小文字化
 ext=$(tr "[:upper:]" "[:lower:]" <<< $ext)
 type=$(tr "[:upper:]" "[:lower:]" <<< $type)
 
 #ファイルの存在確認
-if [ -e $filepath ]; then
+if [ -e "${filepath}" ]; then
 	:
 else
-	echo "$filepath が存在しません!"
+	echo "$filepath が存在しません!\n"
 	exit 0
 fi
 
@@ -24,6 +24,9 @@ if [ ${ext} = "jpeg" ]; then
 	ext="jpg"
 fi
 
+#echo $type
+#echo $ext
+
 #画像以外は除外
 if [ ${type} = "jpg" ]; then
 	:
@@ -32,15 +35,12 @@ elif [ ${type} = "png" ]; then
 elif [ ${type} = "gif" ]; then
 	:
 else
-	echo "$filepath が画像ではありません!"
+	echo "$filepath が画像ではありません!\n"
 	exit 0
 fi
-
-#echo $type
-#echo $ext
 
 if [ ${ext} = ${type} ]; then
 	:
 else
-	echo "$filepath の拡張子が間違っています!"
+	echo "$filepath の拡張子が間違っています!\n"
 fi
